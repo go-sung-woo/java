5006f492b30310763dfbe5b74b718454504cf50b
go-sung-woo <104344426+go-sung-woo@users.noreply.github.com>
2022-05-09 16:30:04 +0900

    Add files via upload

---
+++ ForTest/ForExample01.java
@@ -0,0 +1,12 @@
+
+public class ForExample01 {
+
+	public static void main(String[] args) {
+		// for ¹Ýº¹¹®
+		for(int i = 1; i<=10 ;i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
---
+++ ForTest/ForExample04.java
@@ -0,0 +1,13 @@
+
+public class ForExample04 {
+
+	public static void main(String[] args) {
+		// 반복문 구구단 2단만 출력
+		for(int m = 1 ; m<=9 ; m++) {
+			System.out.printf("\n### %d단 ###\n",m);
+			for(int n)
+			System.out.printf("2 * %d = %d\n", m, 2*m);
+		}
+	}
+
+}
